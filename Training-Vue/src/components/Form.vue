d0c537669ecde2d7ba176bfbc073227d18a2f7e3
Loïc CAILLÉ <148439946+l0l0o@users.noreply.github.com>
2024-06-06 10:48:19 +0200

    Form created

---
+++ Training-Vue/src/components/Form.vue
@@ -0,0 +1,64 @@
+<script></script>
+<template>
+  <div class="container">
+    <form>
+      <div class="mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="eventTitle"
+          placeholder="Titre de l'évènement"
+        />
+      </div>
+
+      <div class="mb-3">
+        <input
+          list="event-type"
+          id="eventType"
+          name="eventType"
+          placeholder="Type de l'évènement"
+        />
+
+        <datalist id="event-type">
+          <option value="Festif"></option>
+          <option value="Conférence"></option>
+          <option value="Sport"></option>
+        </datalist>
+      </div>
+
+      <div class="mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="postAuthor"
+          placeholder="Votre nom"
+        />
+      </div>
+
+      <div class="mb-3">
+        <input type="date" class="form-control" id="eventDate" />
+      </div>
+
+      <div class="mb-3">
+        <input
+          type="textarea"
+          class="form-control"
+          id="eventDescription"
+          placeholder="Description de l'évènement"
+        />
+      </div>
+
+      <div class="mb-3">
+        <input
+          type="text"
+          class="form-control"
+          id="eventImage"
+          placeholder="url de l'image de l'évènement"
+        />
+      </div>
+
+      <button type="submit" class="btn btn-primary">Ajouter</button>
+    </form>
+  </div>
+</template>
+<style></style>
